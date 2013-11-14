2cc84609516865f1134c3b0e8e4b66b9ecce87bc
Rachael L Moore <git@morewry.me>
2013-11-14 01:24:41 -0500

    add new options
    Add new options for multiOpen, multiTarget.  Update data passed to event
    handler to access instance by name (no more attaching to window).
    Update extension options (1) replace entire default (2) add to behavior
    with optional after fn (3) trigger event.  Update docs.

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ Plugin to toggle the display of a target el. Generic enough to use for tabs, acc
 
 toggleTarget requires a control and a target to show/hide. It will work for dropdown menus, accordions, tabs, and pretty much any scenario in which you want clicking one element to show/hide another.  For the plugin to work, jQuery must be loaded and available in the plugin's scope.  Then the plugin must be instantiated either on the window or on a jQuery object corresponding to a DOM element.
 
-toggleTarget has a loose dependency on stateClasses (another plugin I wrote).  It borrows the same inline HTML configuration syntax and when used together manages all states for toggle controls (normal, hover/focus, active).
+toggleTarget has a loose dependency on [stateClasses](https://github.com/morewry/stateClasses) (another plugin I wrote).  When used together manages states for toggle controls (normal, hover/focus, active).
 
 ## Why to Use?
 
-- Optionally integrates with stateClasses, which can be used to unify state management site-wide.
+- Optionally integrates with [stateClasses](https://github.com/morewry/stateClasses), which can be used to unify state management site-wide.
 - Reduces duplication of code related to these common interface behaviors.
 - Reduces the number of potential failure points where the JavaScript is directly managing the style of elements rather than their behavior.
 - Ensures design teams can manage appearance without needing to edit JavaScript by using CSS animations by default.
@@ -21,9 +21,8 @@ toggleTarget has a loose dependency on stateClasses (another plugin I wrote).  I
 
 When the plugin initiates it will automatically determine toggled state of controls and content based on any classes that are already set on the element(s) in question.
 
-	// Example init 1. On window.
-	$.fn.toggleTarget({
-		groupSelector: '[data-tabsnav]',
+	// Example init 1.
+	$('[data-tabsnav]').toggleTarget({
 		toggleSelector: '[data-tab]',
 		alwaysOne: true
 	});
@@ -50,9 +49,10 @@ When the plugin initiates it will automatically determine toggled state of contr
 	</div>
 
 The above will make all toggles in `data-tabsnav` exclusive.  Meaning only one can show at a time.  It will also ensure one piece of toggle-able content always shows.
-This example also shows a scenario in which the plugin instance will be attached to the window object.  If several objects are using the same HTML, you should only have to invoke the plugin once.
 
-	// Example init 3. On a jQuery object.
+This example also shows a scenario in which the selectors are customized so that different options can be passed to a group.  If several objects are using the same HTML and same configuration, you should only have to invoke the plugin once.
+
+	// Example init 2. On a jQuery object.
 	$('[data-tog]').toggleTarget();
 
 	<button class="menu--btn" data-tog data-target="dropdown">Dropdown Menu</button>
@@ -64,7 +64,7 @@ This example also shows a scenario in which the plugin instance will be attached
 	</section>
 
 - The above will use all the default options.
-- However, because it was passed a jQuery object and there is no group wrapper, it will use the object returned by $('[data-tog]') as both the toggle.
+- Because it was passed a jQuery object and there is no group wrapper, the object returned by $('[data-tog]') will be both the toggle and group.
 - This toggle will be able to alternate between open and closed states on each click.
 
 ## Options
@@ -73,10 +73,25 @@ This example also shows a scenario in which the plugin instance will be attached
 optional, boolean<br>
 default: `false`
 
-One of two behavior profiles for the plugin.  If false, all the toggle-able content in a group can be toggled off (everything can be closed).  If true, there will always be one piece of content visible.
+One of two behavior profiles for the plugin.  By default, all the toggle-able content in a group can be toggled off (everything can be closed).  If true, there will always be one piece of content visible.
 
 - "false" is a good profile for menus.
 - "true" is a good profile for tabs.
+
+**multiOpen**<br>
+optional, boolean<br>
+default: `false`
+
+One of two behavior profiles for the plugin.  By default, only one piece of toggle-able content will display at a time.  If true, all the toggle-able content can display at once.
+
+- "false" is a good profile for tabs.
+- "true" is a good profile for menus.
+
+**multiTarget**<br>
+optional, boolean<br>
+default: `false`
+
+One of two behavior profiles for the plugin.  By default, only each toggle must match up to a single piece of content.  If true, a toggle can display or hide multiple pieces of content.  If **multiOpen** is true, **multiTarget** *should not* be true.
 
 **toggleSelector**<br>
 optional, selector string<br>
@@ -90,17 +105,23 @@ default: `[data-toggles]`
 
 Selector used to find if there is a group of related toggle controls in the DOM.
 
+**targetAttribute**<br>
+optional, string attribute name<br>
+default: `data-target`
+
+Attribute whose value will be substituted for `{{target}}` in **targetSelector**.
+
 **targetSelector**<br>
 optional, selector string with token<br>
 default: `[data-id="{{target}}"]`
 
-Selector used to find toggle-able content in DOM.  Use {{target}} to as a placeholder for the dynamic part of the selector coded into data-target.
+Selector used to find toggle-able content in DOM.  Use {{target}} to as a placeholder for the dynamic part of the selector provided as the value of a **targetAttribute**.
 
 **groupEl**<br>
 optional, selector string or object<br>
 default: none
 
-DOM element node or jQuery object corresponding with a wrapper for the group if there is group of related toggle controls in the DOM.  If you already have a reference to the element, you can pass it in rather than requiring the plugin to re-select it.  Overrides toggleSelector.
+DOM element node or jQuery object corresponding with a wrapper for the group if there is group of related toggle controls in the DOM.  If you already have a reference to the element, you can pass it in rather than requiring the plugin to re-select it.  Overrides **targetSelector**.
 
 **targetShowClass**<br>
 optional, class string<br>
@@ -114,26 +135,42 @@ default: `hidden`
 
 The plugin does not do any animation, rather it assumes css animations for convenience and performance reasons.  Class(es) to add when hiding content.
 
-**targetShowCallback:**<br>
+**targetShowCallback**<br>
 optional, callback function<br>
-default: `function`
+default: `undefined`
 
-Callback when toggle-able content is being shown.  This is one of the places the loose dependency on stateClasses can be removed or JavaScript animation can be added.
+Callback when toggle-able content is being shown.  This is one of the places the loose dependency on [stateClasses](https://github.com/morewry/stateClasses) can be removed or JavaScript animation can be added.
 
-- Instead of changing classes for toggle control state directly, the default callback function triggers the stateClasses event.
-- Instead of animating the transition in JavaScript, the plugin swaps out the optional targetHideClass and targetShowClass, allowing use of CSS animations.
+- Instead of changing classes for toggle control state directly, the default callback function triggers the [stateClasses](https://github.com/morewry/stateClasses) event.
+- Instead of animating the transition in JavaScript, the plugin swaps out the optional **targetHideClass** and **targetShowClass**, allowing use of CSS animations.
+
+To *completely override* this default functionality, pass in a custom **targetShowCallback**.
+
+**targetAfterShow**<br>
+optional, callback function<br>
+default: `undefined`
+
+Callback to extend functionality when toggle-able content is being shown.  Either add an event handler for the `active.toggleTarget` event on the toggle-able content or pass in a custom **targetAfterShow**.
 
 **targetHideCallback**<br>
 optional, callback function<br>
-default: `function`
+default: `undefined`
 
-Callback when toggle-above content is being hidden.  This is one of the places the loose dependency on stateClasses can be removed or JavaScript animation can be added.
+Callback when toggle-able content is being hidden.  This is one of the places the loose dependency on [stateClasses](https://github.com/morewry/stateClasses) can be removed or JavaScript animation can be added.
 
-- Instead of changing classes for toggle control state directly, the default callback function triggers the stateClasses event.
-- Instead of animating the transition in JavaScript, the plugin swaps out the optional targetHideClass and targetShowClass, allowing use of CSS animations.
+- Instead of changing classes for toggle control state directly, the default callback function triggers the [stateClasses](https://github.com/morewry/stateClasses) event.
+- Instead of animating the transition in JavaScript, the plugin swaps out the optional **targetHideClass** and **targetShowClass**, allowing use of CSS animations.
+
+To *completely override* this default functionality, pass in a custom **targetHideCallback**.
+
+**targetAfterHide**<br>
+optional, callback function<br>
+default: `undefined`
+
+Callback to extend functionality when toggle-able content is being hidden.  Either add an event handler for the `normal.toggleTarget` event on the toggle-able content or pass in a custom **targetAfterHide**.
 
 ## Methods
 
 There are no methods designed to be public, but any method could be accessed directly using the standard syntax for jQuery plugins.
 
-	$(selector).toggleTarget(medthodName);
+	$object.toggleTarget(methodName);
--- jquery.toggletarget.js
+++ jquery.toggletarget.js
@@ -1,12 +1,12 @@
 /*
  *
  * jQuery toggleTarget
- * Rachael L Moore @morewry
+ * Andrew Aponte, Rachael L Moore
  *
  * via jQuery Plugin Boilerplate http://jqueryboilerplate.com/
  * via UMD (Universal Module Definition) https://github.com/umdjs/umd
  *
- * Optional dependency on jquery.stateClasses (can override)
+ * Loose dependency on jquery.stateClasses
  *
  */
 (function (factory) {
@@ -17,122 +17,207 @@
 	}
 }(function ($) {
 	var pluginName = "toggleTarget",
-	defaults = {
-		alwaysOne: false,
-		toggleSelector: "[data-tog]",
-		groupSelector: "[data-toggles]",
-		// use {{target}} token to represent a dynamic selector
-		targetSelector: '[data-id="{{target}}"]',
-		// optional, pass in dom object for group
-		groupEl: false,
-		// assumes css animations - class to add when showing target
-		targetShowClass: '',
-		// assumes css animations - class to add when removing content
-		targetHideClass: 'hidden',
-		// default callbacks
-		targetShowCallback: function ( event, each ) {
-			// Trigger active state for toggle assumes plugin jquery.stateClasses.
-			// You can pass a custom targetShowCallback -- or set up your own event handler.
-			each.$this.trigger("active");
-			// Toggle for target assumes css animation, only changes class to show/hide
-			// You can pass a custom targetShowCallback
-			each.$target.removeClass(each.data.settings.targetHideClass);
-			each.$target.addClass(each.data.settings.targetShowClass);
-		},
-		targetHideCallback: function( event, each ) {
-			// Trigger normal state for toggle assumes plugin jquery.stateClasses.
-			// You can pass a custom targetHideCallback -- or set up your own event handler.
-			each.$this.trigger("normal");
-			// Toggle for target assumes css animation, only changes class to show/hide
-			// You can pass a custom targetHideCallback
-			each.$target.addClass(each.data.settings.targetHideClass);
-			each.$target.removeClass(each.data.settings.targetShowClass);
-		}
-	};
+			defaults = {
+				alwaysOne: false,
+				multiOpen: false,
+				multiTarget: false,
+				toggleSelector: '[data-tog]',
+				groupSelector: '[data-toggles]',
+				// use {{target}} token to represent a dynamic selector
+				targetAttribute: 'data-target',
+				targetSelector: '[data-id="{{target}}"]',
+				// optional, only for passing in pre-selected node for group
+				groupEl: false,
+				// assumes css animations - class to add when showing target
+				targetShowClass: '',
+				// assumes css animations - class to add when removing content
+				targetHideClass: 'hide'
+				// optional callbacks
+				/**
+				 *
+				 * Target ____ Callback / Target After ____
+				 * arguments: "event" is jquery event object
+				 * 						"opts" are plugin instance options
+				 * context: "this" is an individual toggle object
+				 * { el, $el, $target }
+				 *
+				 * Use target____Callback to override default show/hide functionality
+				 * Use targetAfter____ to add to default show/hide functionality
+				 *
+				**/
+				// targetShowCallback: function ( event, opts ) { },
+				// targetAfterShow: function ( event, opts ) { },
+				// targetHideCallback: function( event, opts ) { },
+				// targetAfterHide: function ( event, opts ) { }
+			}, // defaults
+			$document = $(document);
 	function Plugin ( el, options ) {
-		// extend instance settings with passed-in options
-		this.settings = $.extend( {}, defaults, options );
-		// mostly for neatness, set groupSelector to selector (if any)
-		if ( options.selector ) { this.settings.groupSelector = options.selector }
+		this._defaults = defaults;
+		this._name = pluginName;
+		this._state = this._name + '-state';
+		this.opts = $.extend( {}, defaults, options );
 		// several options for el, figure out which should be used for this instance
-		if ( this.settings.groupEl ) {
+		if ( this.opts.groupEl ) {
 			// el passed in as optional groupEl
-			this.el = this.settings.groupEl;
-			this.$el = $(this.settings.groupEl);
-			this.$togs = this.$el.find(this.settings.toggleSelector);
-		}
+			this.el = this.opts.groupEl;
+			this.$el = $(this.opts.groupEl);
+			this.$togs = this.$el.find(this.opts.toggleSelector);
+		} // groupEl
 		else if ( el.nodeType == 1 ) {
 			// el passed in jquery plugin style
 			this.el = el;
 			this.$el = $(el);
-			this.$togs = this.$el.find(this.settings.toggleSelector);
-			if ( !this.$togs.length ) {
-				this.$togs = this.$el;
-				// there is no group, set toggleSelector to selector (if any)
-				if ( options.selector ) { this.settings.toggleSelector = options.selector }
-			}
-		}
-		else if ( this.settings.groupSelector ) {
+			this.$togs = this.$el.find(this.opts.toggleSelector);
+			if ( !this.$togs.length ) { this.$togs = this.$el; }
+		} // nodeType == 1
+		else if ( this.opts.groupSelector ) {
 			// no el passed in, use groupSelector
-			this.$el = $(this.settings.groupSelector);
+			this.$el = $(this.opts.groupSelector);
 			this.el = this.$el[0];
-			this.$togs = this.$el.find(this.settings.toggleSelector);
-		}
+			this.$togs = this.$el.find(this.opts.toggleSelector);
+		} // groupSelector
 		else {
-			$.error("jquery.toggletarget.js - unable to determine el, so unable to determine toggles");
+			$.error("$.fn.toggleTarget - unable to determine el, so unable to determine toggles");
 		}
-		this._defaults = defaults;
-		this._name = pluginName;
 		this.init();
 	}
 	Plugin.prototype = {
 
+		/**
+		 *
+		 * init
+		 *
+		**/
 		init: function () {
-			var eventName = 'click' + '.' + this._name + '.' + this.settings.groupSelector.replace(/[^a-z0-9-]/gi,'') + '.' + this.settings.toggleSelector.replace(/[^a-z0-9-]/gi,'');
-			$(document).off(eventName).on(
-				eventName,
-				this.settings.toggleSelector,
-				this,
-				this.toggleTarget
-			);
+			// console.log( "$.fn.toggleTarget - init" );
+			// console.log(this.opts);
+			var eventName = 'click.' + this._name,
+					eventHandler = function ( event ) {
+						// console.log("$.fn.toggleTarget - eventHandler, key: " + event.data.key);
+						// console.log(event);
+						var isPlugin = function ( o, k ) {
+									return ( o && typeof o == "object" && o._toggle );
+								},
+								el = event.currentTarget,
+								pel = (el) ? $.data(el, event.data.key) : event.delegateTarget,
+								plugin = (pel && !isPlugin(pel)) ? $.data(pel, event.data.key) : pel;
+						return (isPlugin(plugin)) ? plugin._toggle( el, event ) : false;
+					};
+			// console.log("$.fn.toggleTarget - on " + eventName + " " + this.opts.toggleSelector);
+			$document.on(eventName, this.opts.toggleSelector, {key: this._name}, eventHandler);
+			this.$togs.data(this._name, this.el);
 		}, // init
 
-		toggleTarget: function ( event ) {
+		/**
+		 *
+		 * _toggle
+		 *
+		**/
+		_toggle: function ( el, event ) {
+			// console.log("$.fn.toggleTarget - _toggle");
+			// console.log(el);
+			var plugin = this, cur = {}, target, temp;
 			// Refs to clicked el, its siblings
-			event.data.$this = $(event.currentTarget);
-			event.data.$sibs = event.data.$togs.not(event.data.$this);
+			cur.$el = $(el);
+			cur.$sibs = plugin.$togs.not(cur.$el);
 			// Toggle state
-			event.data.state = ( event.data.settings.alwaysOne ) ? true : !(event.data.$this.data("tog"));
+			cur.state = ( plugin.opts.alwaysOne ) ? true : !(cur.$el.data(plugin._state));
 			// Set event origin toggler to true / active
-			event.data.$this.data("tog", event.data.state);
+			cur.$el.data(plugin._state, cur.state);
 			// Set sibling toggles to false / inactive
-			event.data.$sibs.data("tog", false);
+			cur.$sibs.data(plugin._state, false);
 			// For each possible toggle
-			$.each(event.data.$togs, function ( i ) {
+			$.each(plugin.$togs, function ( i ) {
 				// Refs to current toggle, its target
-				var each = { data: event.data };
-				each.$this = $(this),
-				each.$target = $(event.data.settings.targetSelector.replace("{{target}}", each.$this.data("target")));
-				// If this toggle is currently active
-				if( !!each.$this.data("tog") ) {
-					// Call active callback
-					event.data.settings.targetShowCallback( event, each );
+				var each = {
+					el: this,
+					$el: $(this),
+					$target: {}
+				};
+				target = plugin.opts.targetSelector.replace("{{target}}", each.el.getAttribute(plugin.opts.targetAttribute));
+				each.$target = $(target);
+				if ( !plugin.opts.multiTarget && each.$target.length > 1 ) {
+					// console.log(i);
+					each.$target = each.$target.eq(i);
 				}
+				// If this toggle is currently active
+				if( !!each.$el.data(plugin._state) ) {
+					// Call active callback
+					plugin._show(each, event);
+				} // active
 				// If this toggle IS NOT ACTIVE
 				else {
-					// Call normal callback
-					event.data.settings.targetHideCallback( event, each );
-				}
-			}); // each event.data.$togs
-		} // toggleTarget
+					// If more than one shouldn't be open at a time
+					if ( !plugin.opts.multiOpen ) {
+						// Call normal callback
+						plugin._hide(each, event);
+					} // !multiOpen
+				} // inactive
+			}); // each plugin.$togs
+		}, // toggle
+
+		/*
+		 * _show
+		 * tog = { el, $el, $target }
+		 * Pass in a custom targetShowCallback to override
+		 * Extend _show by passing in a custom targetAfterShow
+		 * Or by adding an event listener for active.toggleTarget
+		*/
+		_show: function ( tog, event ) {
+			var after = function ( t, e ) {
+				t.$target.trigger('active.' + this._name);
+				if ( this.targetAfterShow && typeof this.targetAfterShow == "function" ) { this.targetAfterShow.call(t, e, this.opts); }
+			};
+			if ( this.targetShowCallback && typeof this.targetShowCallback == "function" ) {
+				this.targetShowCallback.call(tog, event, this.opts);
+				after.call(this, tog, event);
+				return;
+			}
+			// Trigger active state for toggle assumes plugin jquery.stateClasses
+			tog.$el.trigger('active');
+			// Toggle for target assumes css animation, only changes class to show/hide
+			tog.$target.removeClass(this.opts.targetHideClass);
+			tog.$target.addClass(this.opts.targetShowClass);
+			// Extendable event, optional after callback
+			after.call(this, tog, event);
+		}, // _show
+
+		/*
+		 * _hide
+		 * tog = { el, $el, $target }
+		 * Pass in a custom targetHideCallback to override
+		 * Extend _hide by passing in a custom targetAfterHide
+		 * Or by adding an event listener for normal.toggleTarget
+		*/
+		_hide: function( tog, event ) {
+			var after = function ( t, e ) {
+				t.$target.trigger('normal.' + this._name);
+				if ( this.targetAfterHide && typeof this.targetAfterHide == "function" ) { this.targetAfterHide.call(t, e, this.opts); }
+			};
+			if ( this.targetHideCallback && typeof this.targetHideCallback == "function" ) {
+				this.targetHideCallback.call(tog, event, this.opts);
+				after.call(this, tog, event);
+				return;
+			}
+			// Trigger normal state for toggle assumes plugin jquery.stateClasses
+			tog.$el.trigger('normal');
+			// Toggle for target assumes css animation, only changes class to show/hide
+			tog.$target.addClass(this.opts.targetHideClass);
+			tog.$target.removeClass(this.opts.targetShowClass);
+			// Extendable event, optional after callback
+			after.call(this, tog, event);
+		} // _hide
 
 	}; // prototype
 	$.fn[ pluginName ] = function ( options ) {
-		var _this = this, instance, instantiate, returns, options = options || {};
-		instantiate = function () {
+		// console.log("$.fn.toggleTarget");
+		var instance, returns, options = options || {};
+		if ( this.selector && !options.selector ) {
+			$.extend( { groupSelector: this.selector, toggleSelector: this.selector }, options );
+		}
+		returns = this.each(function () {
 			instance = $.data( this, pluginName );
-			if ( typeof options === 'object' && ( !instance || this.self == window ) ) {
-				if ( _this.selector && _this.selector.length ) { options.selector = _this.selector; }
+			if ( typeof options === 'object' && !instance ) {
 				// Instantiate plugin
 				$.data( this, pluginName, new Plugin( this, options ) );
 			}
@@ -140,9 +225,8 @@
 				// Directly call method
 				returns = instance[options].apply( instance, Array.prototype.slice.call( arguments, 1 ) );
 			}
-		};
-		if ( this instanceof jQuery ) { returns = this.each(instantiate); }
-		else { returns = instantiate(); }
+		});
 		return returns;
 	}; // $.fn
 }));
+
